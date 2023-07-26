e89139c304d3dfb29fb245be25e1253138044408
Leonardo Emanuel Salas <emanuelsalasl20@gmail.com>
2023-07-25 21:09:42 -0300

    se agrega carrito

--- mine.js
+++ mine.js
@@ -1,33 +1,61 @@
-/* precios de los servicios */
 const servicios = {
-    botox: 100,
-    acido: 200,
-    vitaminaC: 300
-  };
-  
-/* Función para calcular el precio con descuento */
-  function calcularPrecioConDescuento(precio, descuento) {
-    const porcentajeDescuento = descuento / 100;
-    const montoDescuento = precio * porcentajeDescuento;
-    const precioConDescuento = precio - montoDescuento;
-    return precioConDescuento;
-  }
-  
-/*   Obtener el servicio seleccionado y el descuento del usuario */
-  const servicioSeleccionado = prompt("Ingrese el servicio seleccionado (botox, acido o vitaminaC):");
-  const descuento = parseFloat(prompt("Ingrese el porcentaje de descuento:"));
-  
-/* Obtener el precio del servicio seleccionado */
-  const precioServicio = servicios[servicioSeleccionado];
-  
-/* Verificar si el servicio existe en el objeto de precios */
-  if (precioServicio !== undefined) {
+  botox: 100,
+  acido: 200,
+  vitaminaC: 300,
+};
 
-/* Calcular el precio con descuento */
-    const precioConDescuento = calcularPrecioConDescuento(precioServicio, descuento);
-  
-/* Mostrar el resultado */
-    console.log("El precio del servicio seleccionado con descuento es: $" + precioConDescuento.toFixed(2));
-  } else {
-    console.log("El servicio seleccionado no es válido.");
-  }
+const productos = [];
+
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajeDescuento = descuento / 100;
+  const montoDescuento = precio * porcentajeDescuento;
+  const precioConDescuento = precio - montoDescuento;
+  return precioConDescuento;
+}
+
+function agregarProducto(modelo, precio) {
+  const producto = {
+    modelo: modelo,
+    precio: precio,
+  };
+  productos.push(producto);
+}
+
+function buscarProductoPorModelo(modelo) {
+  return productos.find((producto) => producto.modelo === modelo);
+}
+
+function buscarProductosPorPrecio(precio) {
+  return productos.filter((producto) => producto.precio === precio);
+}
+
+function calcularPrecioTotalCarrito() {
+  let total = 0;
+  productos.forEach((producto) => {
+    total += producto.precio;
+  });
+  return total;
+}
+
+// Agregar productos de estética facial
+agregarProducto("cremaFacial", 50);
+agregarProducto("mascarilla", 25);
+
+// Mostrar cuadro de precios y valor del carrito
+function mostrarCuadroPrecios() {
+  console.log("---- Precios ----");
+  Object.entries(servicios).forEach(([producto, precio]) => {
+    console.log(`${producto}: $${precio}`);
+  });
+
+  console.log("Productos en el carrito:");
+  productos.forEach((producto) => {
+    console.log(`${producto.modelo}: $${producto.precio}`);
+  });
+
+  const precioTotalCarrito = calcularPrecioTotalCarrito();
+  console.log("---------------------------");
+  console.log(`Total carrito: $${precioTotalCarrito}`);
+}
+
+mostrarCuadroPrecios();
